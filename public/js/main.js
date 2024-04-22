f8d8b8ecc4f8853512f94f284f5c019df2608e3a
Hactchubas <kauamaia@yahoo.com.br>
2024-04-22 17:54:34 -0300

    First Commit

---
+++ public/js/main.js
@@ -0,0 +1,111 @@
+
+let startingTime = $("#remainingTime").text()
+let field = $("#typeField");
+let timeCountId = 0;
+var intervals = [];
+
+$(document).ready(function () {
+    updatePhraseSize();
+
+    // Update character and words event by input on textarea
+    counterInitializer();
+
+    // Timer Event Init
+    timerInitializer();
+
+    // Restart Game Click Event
+    restartGameSetup();
+});
+
+
+// Count game phrase words
+function updatePhraseSize() {
+    let frase = $("#phrase").text();
+    $(".nWords").text(frase.split(" ").length);
+}
+
+
+// Restart game setup
+function restartGameSetup() {
+    $("#restartGameButton").click(() => {
+        field.val("");
+        field.attr("disabled", false);
+
+        $("#remainingTime").text(startingTime);
+        timerInitializer();
+        counterInitializer();
+
+
+        field.removeClass("disabled_typeField");
+    });
+}
+
+
+// Textarea characters and words counter
+function counterInitializer() {
+    $(".charactersTyped").text("0");
+    $(".wordsTyped").text("0");
+    
+    field.on("input", () => {
+        countCharactersWords();
+        checkTextValid();
+    });
+
+}
+
+function countCharactersWords() {
+    let typed = field.val();
+    $(".charactersTyped").text(typed.split("").length);
+    $(".wordsTyped").text(typed.split(/\S+/).length - 1);
+}
+
+function checkTextValid() {
+    let phrase = $("#phrase").text();
+    let typed = field.val();
+    let comparableStr = phrase.substr(0, typed.length);
+
+    field.addClass("incorrect_field");
+    field.removeClass("correct_field");
+    if (comparableStr == typed && typed != "") {
+        field.addClass("correct_field");
+        field.removeClass("incorrect_field");
+    }
+}
+
+
+// Timer count down initializer
+function timerInitializer() {
+    // Guarantee interval end before starting new one
+    myClearIntervals(timeCountId)
+    field.one("focus", () => {
+        myClearIntervals(timeCountId)
+        timeCountId = setInterval(timer, 1000);
+        intervals.push(timeCountId)
+    });
+}
+
+function myClearIntervals(clearId) {
+    intervals.map(clearInterval)
+    const index = intervals.indexOf(clearId);
+    intervals.splice(index, 1);
+}
+
+// Timer count down event
+function timer() {
+    remainingTime = $("#remainingTime").text()
+    if (remainingTime < 1) {
+        field.attr("disabled", true);
+
+        myClearIntervals();
+
+        field.toggleClass("disabled_typeField");
+
+        return
+    }
+    remainingTime--;
+    $("#remainingTime").text(remainingTime);
+
+}
+
+
+
